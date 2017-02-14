37f0e8435064d4128e993b71bb74f6b571772ce1
Dan Sisan <dan.sisan@gmail.com>
2017-02-13 23:31:31 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var elm = require('gulp-elm');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var connect = require('gulp-connect');
+var less = require('gulp-less');
+var path = require('path');
+
+// File paths
+var paths = {
+    dest: 'dist',
+    elm: 'src/*.elm',
+    less: 'src/less/*.less',
+    static: 'src/*.{html,css}'
+};
+
+// Init Elm
+gulp.task('elm-init', elm.init);
+
+// Compile Elm to HTML
+gulp.task('elm', ['elm-init'], function(){
+    return gulp.src(paths.elm)
+        .pipe(plumber())
+        .pipe(elm())
+        .pipe(gulp.dest(paths.dest));
+});
+
+// Compile less
+gulp.task('less', function () {
+    return gulp.src('src/less/*.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest(paths.dest));
+});
+
+// Move static assets to dist
+gulp.task('static', function() {
+    return gulp.src(paths.static)
+        .pipe(plumber())
+        .pipe(gulp.dest(paths.dest));
+});
+
+// Watch for changes and compile
+gulp.task('watch', function() {
+    gulp.watch(paths.elm, ['elm']);
+    gulp.watch(paths.less, ['less']);
+    gulp.watch(paths.static, ['static']);
+});
+
+// Local server
+gulp.task('connect', function() {
+    connect.server({
+        root: 'dist',
+        port: 3000
+    });
+});
+
+// Main gulp tasks
+gulp.task('build', ['elm', 'less', 'static']);
+gulp.task('default', ['connect', 'build', 'watch']);
